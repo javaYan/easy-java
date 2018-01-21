6fef69de5e649edfd999602ee6821c351bb2a3f2
javaYan <834595596@qq.com>
2018-01-21 20:36:47 +0800

    update ClassTest

---
+++ easy-practice/src/main/java/easy/practice/StudentPrivateService.java
@@ -0,0 +1,32 @@
+package easy.practice;
+
+/**
+ * Created by Mr_yyy on 2018/1/21.
+ */
+public class StudentPrivateService {
+    /**
+     * 私有构造器
+     */
+    private StudentPrivateService() {}
+    private String name;
+
+    public void say() {
+        System.out.println("say...");
+    }
+
+    public void doSay() {
+        System.out.println("do say...");
+    }
+
+    public void introduce() {
+        System.out.println("my name is " + name);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
--- easy-practice/src/test/java/easy/practice/ClassTest.java
+++ easy-practice/src/test/java/easy/practice/ClassTest.java
@@ -19,22 +19,42 @@ public class ClassTest {
      */
     @Test
     public void testInvoke() throws NoSuchMethodException, IllegalAccessException, InstantiationException, InvocationTargetException {
-        Class studentServiceClass = StudentService.class;
-        Method sayMethod = studentServiceClass.getMethod("say");
-        Method doSayMethod = studentServiceClass.getMethod("doSay");
-        Object obj = studentServiceClass.newInstance();
+        Class studentServiceCls = StudentService.class;
+        Method sayMethod = studentServiceCls.getMethod("say");
+        Method doSayMethod = studentServiceCls.getMethod("doSay");
+        Object obj = studentServiceCls.newInstance();
         sayMethod.invoke(obj);
         doSayMethod.invoke(obj);
     }
 
+    /**
+     * field
+     * @throws InvocationTargetException
+     * @throws IllegalAccessException
+     * @throws NoSuchMethodException
+     * @throws NoSuchFieldException
+     * @throws InstantiationException
+     */
     @Test
     public void testField () throws InvocationTargetException, IllegalAccessException, NoSuchMethodException, NoSuchFieldException, InstantiationException {
-        Class studentServiceClass = StudentService.class;
-        Object obj = studentServiceClass.newInstance();
-        Field nameField = studentServiceClass.getDeclaredField("name");
+        Class studentServiceCls = StudentService.class;
+        Object obj = studentServiceCls.newInstance();
+        Field nameField = studentServiceCls.getDeclaredField("name");
         nameField.setAccessible(true);
         nameField.set(obj, "Mr_yyy");
-        Method introduceMethod = studentServiceClass.getMethod("introduce");
+        Method introduceMethod = studentServiceCls.getMethod("introduce");
         introduceMethod.invoke(obj);
+    }
+
+    /**
+     * Private的构造方法对应的类在newInstance时会抛出异常
+     * @throws IllegalAccessException
+     * @throws InstantiationException
+     */
+    @Test
+    public void testPrivateClass () throws IllegalAccessException, InstantiationException {
+        Class studentPrivateServiceCls = StudentPrivateService.class;
+        Object obj = studentPrivateServiceCls.newInstance();
+        //IllegalAccessException
     }
 }
